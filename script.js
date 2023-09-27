e902cd7d8a9d3ce992d0bfb06e9173885534b741
Euram Dz <145721055+euramdzd@users.noreply.github.com>
2023-09-27 17:07:47 +0200

    script.js

--- script.js
+++ script.js
@@ -12,67 +12,57 @@
 // */
 // // 
 
-// //JEGi3C3GX0Z6O0RuWBbT1TNma1u0z24Af9GzKH6mGs6QoL9tD91SWc6uGU3d
 
-// var today = new Date();
+var today = new Date();
 
-// var Dat_e = ` ${today.getDate()} / ${today.getMonth()+1} / ${today.getFullYear()} `
+var Dat_e = ` ${today.getDate()} / ${today.getMonth()+1} / ${today.getFullYear()} `
 
-// document.querySelector('.date').innerHTML = Dat_e;
+document.querySelector('.date').innerHTML = Dat_e;
   
-// var socket  = new WebSocket('wss://wss.allsportsapi.com/live_events?widgetKey='+APIkey+'&timezone=+01:00');
-// console.log(socket)
-// socket.onmessage = function(e) {
-//   console.log('ok')
-//   if (e.data) {
-//     console.log('ok')
-//     var matchesData = JSON.parse(e.data);
+var socket  = new WebSocket('wss://wss.allsportsapi.com/live_events?widgetKey='+APIkey+'&timezone=+01:00');
+console.log(socket)
+socket.onmessage = function(e) {
+  console.log('ok')
+  if (e.data) {
+    console.log('ok')
+    var matchesData = JSON.parse(e.data);
 
-//     console.log(matchesData);
-//     let match = [];
+    console.log(matchesData);
+    let match = [];
 
-//     for(let i =0;i<matchesData.length;i++){
-//         /* مباريات   */
-//       match[match.length] = [ matchesData[i].league_name , // اسم بطولة
-//                               matchesData[i].league_logo, // شعارها
-//                               matchesData[i].country_logo, // علم دولة
-//                               matchesData[i].event_home_team, // فريق 1 
-//                               matchesData[i].event_away_team, // فريق2  
-//                               matchesData[i].event_status, // وقت
-//                               matchesData[i].event_final_result, // نتيجة
-//                             ]
+    for(let i =0;i<matchesData.length;i++){
+        /* مباريات   */
+      match[match.length] = [ matchesData[i].league_name , // اسم بطولة
+                              matchesData[i].league_logo, // شعارها
+                              matchesData[i].country_logo, // علم دولة
+                              matchesData[i].event_home_team, // فريق 1 
+                              matchesData[i].event_away_team, // فريق2  
+                              matchesData[i].event_status, // وقت
+                              matchesData[i].event_final_result, // نتيجة
+                            ]
       
 
-//     }
+    }
 
-//     let box = '';
+    let box = '';
 
-//     for(let i=0;i<match.length;i++){
-//       box  += `
-//       <div class="box">
-//       <img src="${match[i][2]}" class="country"/> 
-//       <div class="logo"><img src="${match[i][1]}"/> </div><p>${match[i][0]}</p>
-//       <div class="Time">${match[i][5]}</div>
-//       <div class="details">
-//         <p>${match[i][3]}</p>
-//         <p>${match[i][6]}</p>
-//         <p>${match[i][4]}</p>
-//       </div>
-//       </div>
-//       `
-//     }
-//     document.querySelector("#match").innerHTML = box;
-//   }else{
-//     console.log("err ...!")
-//   }
-// }
+    for(let i=0;i<match.length;i++){
+      box  += `
+      <div class="box">
+      <img src="${match[i][2]}" class="country"/> 
+      <div class="logo"><img src="${match[i][1]}"/> </div><p>${match[i][0]}</p>
+      <div class="Time">${match[i][5]}</div>
+      <div class="details">
+        <p>${match[i][3]}</p>
+        <p>${match[i][6]}</p>
+        <p>${match[i][4]}</p>
+      </div>
+      </div>
+      `
+    }
+    document.querySelector("#match").innerHTML = box;
+  }else{
+    console.log("err ...!")
+  }
+}
   
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch("https://api.sportmonks.com/v3/core/countries", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
